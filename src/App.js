441c0836e9cb998611fc5df6d983cc5a50a63d59
yeri <webothknow@naver.com>
2021-04-22 22:26:28 +0900

    https://github.com/webothknow/apple.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let [title, settite] = useState([
+    "리액트 포스트",
+    "즐겨찾기순위",
+    "강의사이트추천",
+  ]);
+
+  let [Text, settext] = useState(0);
+
+  let [modal, setmodal] = useState(false);
+
+  let [pushtitle, setpushtitle] = useState(0);
+
+  function titlechange() {
+    let newArray = [...title];
+    newArray = newArray.sort();
+    settite(newArray);
+  }
+
+  let posts = "강남 고기 맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button onClick={titlechange}>버튼</button>
+
+      {title.map(function (a, i) {
+        return (
+          <div className="list">
+            <h3
+              onClick={() => {
+                setpushtitle(i);
+              }}
+            >
+              {a}
+              <span
+                onClick={() => {
+                  settext(Text + 1);
+                }}
+              >
+                👍
+              </span>
+              {Text}
+            </h3>
+            <p>2월 17일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      {modal == true ? <Modal title={title} pushtitle={pushtitle} /> : null}
+      <button
+        onClick={() => {
+          setmodal(!modal);
+        }}
+      >
+        버튼
+      </button>
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h2>{props.title[props.pushtitle]}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
